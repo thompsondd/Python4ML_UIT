8fad06ffdcc9a45404ee575d28ed87fff2a6199e
Dang Laptop <dnng2002@gmail.com>
2022-11-17 08:16:13 +0700

    Add folder

---
+++ Lab_8/Lab_7.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import ml
+
+#st.title("My First Dang's Web")
+
+#1. Markdown
+st.markdown("""
+# Hệ thống AI dự báo
+## 1. Dự báo lương
+""")
+data = st.file_uploader("Tải file dữ liệu về lương để AI dự đoán",key="data")
+setting={}
+if data is not None:
+    byte_data=data.getvalue()
+    with open(f"./data/{data.name}","wb+") as f:
+        f.write(byte_data)
+    
+    # Select input feature
+    dataset = ml.Dataset(f"./data/{data.name}")
+    st.dataframe(dataset.origin_data)
+    get_feature_inputs={}
+    target = dataset.origin_data.columns[-1]
+    st.write("Select features as input features")
+    for i in list(dataset.origin_data.columns)[:-1]:
+        get_feature_inputs.update({i:st.checkbox(f"{i}",key=f"feature_select_{i}")})
+    
+    # Select output feature
+    st.write(f"Output features: {target}")
+
+    # Select K-Fold
+    kfold = st.checkbox(f"K-Fold Cross validation",key="kfold")
+    setting.update({"kfold":kfold})
+    if kfold:
+        K = st.number_input("Enter K",key="rate_train", step=1, min_value=2, value=2)
+        setting.update({"K":K})
+
+    # Select the rate of train dataset
+    if kfold == False:
+        rate = st.number_input("Enter the rate of train dataset",key="rate_train", value=0.1, min_value=0.1, max_value=1.0, step=0.01)
+        setting.update({"rate":rate})
+
+    # Select metric
+    st.write("Select at least one metric for evaluating model")
+    setting.update({"F1":st.checkbox(f"F1",key="F1")})
+    setting.update({"LogLoss":st.checkbox(f"LogLoss",key="LogLoss")})
+    
+    if st.button("Run",key="run"):
+        if (setting["F1"] or setting["LogLoss"])==False:
+            st.error("Please select a metric for evaluation")
+        else:
+            feature_selected = [ i for i in get_feature_inputs.keys() if get_feature_inputs[i]]
+            if len(feature_selected)<1:
+                st.error("Please select at least a feature")
+            else:
+                setting.update({"feature_list":feature_selected,"target":target})
+                model = ml.Model_AI(dataset, setting)
+                model.fit()
+                #h = pd.DataFrame(model.history)
+                #st.dataframe(h)
+                st.pyplot(model.plot_history())
+        #for i in get_inputs:
+        #    if i not in model_salary.str_col:
+        #        get_inputs[i]=float(get_inputs[i])
+        ##st.write(f"Input={get_inputs}")
+        ##st.write(f"Best model={model.best_model}")
+        #st.success(f"Salary = {model_salary.predict(get_inputs)}")
---
+++ Lab_8/ml.py
@@ -0,0 +1,157 @@
+import os
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, log_loss, f1_score
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import Normalizer
+import matplotlib.pyplot as plt
+
+class OnehotEncoder:
+    def __init__(self):
+        self.lenght=0
+        self.data={}
+    def fit(self,data):
+        for id,key in enumerate(data):
+            self.data[key]=id
+        self.lenght = len(data)
+    def transform(self,data):
+        ba = []
+        for i in data:
+            a = np.zeros(self.lenght,dtype=np.int32)
+            for j in i:
+                a[self.data[j]]=1
+            ba.append(a.tolist())
+        return ba
+
+class Dataset:
+    def __init__(self,path_data):
+        self.data=None
+        self.encoder={}
+        self.features = []
+        self.str_col=[]
+        self.origin_data = pd.read_csv(path_data)
+        if len(self.origin_data.columns)==1:
+            self.origin_data = pd.read_table(path_data,sep=";")
+
+    def check_str_type(self, word):
+        return str(type(word)).split()[1].split("'")[1] == "str"
+    def process_data(self, data):
+        self.data = data.copy()
+        self.features.append(list(self.data.columns))
+        self.target = self.data.columns[-1]
+        for i in self.data.columns:
+            if self.check_str_type(self.data.iloc[0][i]):
+                LaEn = OnehotEncoder()
+                temp = self.data[i].unique()
+                LaEn.fit(temp)
+                self.str_col.append(i)
+                temp_data = np.array(LaEn.transform(self.data[i].values.reshape((-1,1)))).T
+                for id, key in enumerate(temp):
+                    self.data[key]=temp_data[id]
+                self.encoder.update({i:LaEn})
+                self.data.drop(i,axis=1,inplace=True)
+        return self.target, list(filter(lambda x: x!=self.target, self.data.columns))
+    def get_feature_target(self,features_list,target):
+        features_list+=[target]
+        target_col,feature_col = self.process_data(self.origin_data[features_list])
+        return self.data[feature_col].values, self.data[[target_col]].values
+class Model_AI:
+    def __init__(self,dataset,setting):
+        self.data= dataset
+        self.setting = setting
+        self.history = {}
+        if self.setting["LogLoss"]:
+            self.history["LogLoss"]={}
+        if self.setting["F1"]:
+            self.history["F1"]={}
+
+    def fit(self):
+        X,y = self.data.get_feature_target(self.setting["feature_list"],self.setting["target"])
+        if self.setting["kfold"]:
+            kf = KFold(n_splits=self.setting["K"])
+            fold_id=0
+            for train_index, test_index in kf.split(X):
+                xtrain, xtest = X[train_index], X[test_index]
+                ytrain, ytest = y[train_index], y[test_index]
+
+                Model = LogisticRegression()
+                Model.fit(xtrain,ytrain)
+                yhat = Model.predict(xtest)
+                yhat_p = Model.predict_proba(xtest)
+                #print(yhat)
+                if self.setting["LogLoss"]:
+                    self.history["LogLoss"].update({fold_id:log_loss(ytest,yhat_p)})
+                if self.setting["F1"]:
+                    self.history["F1"].update({fold_id:f1_score(ytest,yhat, average='weighted')})
+                fold_id+=1
+        else:
+            xtrain,xtest,ytrain,ytest = train_test_split(X,y, test_size = 1-self.setting["rate"])
+            Model = LogisticRegression()
+            Model.fit(xtrain,ytrain)
+            yhat = Model.predict(xtest)
+            yhat_p = Model.predict_proba(xtest)
+            if self.setting["LogLoss"]:
+                self.history["LogLoss"].update({0:log_loss(ytest,yhat_p)})
+            if self.setting["F1"]:
+                self.history["F1"].update({0:f1_score(ytest,yhat, average='weighted')})
+        self.model = Model
+        print(self.history)
+
+    def plot_history(self):
+        data = pd.DataFrame(self.history)
+        labels = list(data.index)
+        fig, ax = plt.subplots()
+        title = []
+        if len(labels)>1:
+            labels +=["Mean"]
+
+        x = np.arange(len(labels))  # the label locations
+        width = 0.35  # the width of the bars
+        try:
+            F1 = [i for i in data["F1"].values]
+            if "Mean" in labels:
+                F1 +=[np.mean(data["F1"].values)]
+            rects1= ax.bar(x - width/2, F1, width, label='F1', color="green")
+            title.append("F1")
+        except:
+            pass
+
+        try:
+            LogLoss = [i for i in data["LogLoss"].values]
+            if "Mean" in labels:
+                LogLoss += [np.mean(data["LogLoss"].values)]
+            rects2=ax.bar(x + width/2, LogLoss, width, label='LogLoss',color="blue")
+            title.append("LogLoss")
+        except:
+            pass
+        # Add some text for labels, title and custom x-axis tick labels, etc.
+        ax.set_ylabel('Error')
+        #ax.set_yscale("log")
+        ax.set_title(f'Error of {title[0] if len(title)<1 else "and".join(title)}')
+        ax.set_xticks(x, labels)
+        ax.legend()
+
+        #ax.bar_label(rects1, padding=3)
+        #ax.bar_label(rects2, padding=3)
+
+        fig.tight_layout()
+        return fig
+
+    def extract_vector(self,features):
+        feature_vector = []
+        for i in features.keys():
+            if i not in self.str_col:
+                feature_vector.append(features[i])
+        for i in self.str_col:
+            feature_vector.extend(self.encoder[i].transform([[features[i]]])[0])
+        return feature_vector
+
+    def predict(self, features):
+        '''
+            features = { Position:..., Level:...}
+        '''
+        features = self.extract_vector(features)
+        return self.best_model.predict([features]).reshape(1)[0]
+
