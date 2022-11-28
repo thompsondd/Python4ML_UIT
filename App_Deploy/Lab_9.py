0cdc319930927b0ed951f880e217bb7a2261eb20
Dang Laptop <dnng2002@gmail.com>
2022-11-28 08:47:29 +0700

    Create static folder for deploying

---
+++ App_Deploy/Lab_9.py
@@ -0,0 +1,167 @@
+'''
+Sinh viên thực hiện: Nguyễn Huỳnh Hải Đăng
+MSSV: 20521159
+'''
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import ml
+import sklearn.datasets as datasets
+import math
+
+#1. Markdown
+st.markdown("""
+# Hệ thống AI dự báo
+""")
+#data = st.file_uploader("Tải file dữ liệu về lương để AI dự đoán",key="data")
+setting={}
+class count_section:
+    def __init__(self):
+        self.number = 0
+    def __call__(self):
+        self.number+=1
+        return self.number
+data = st.file_uploader("Tải file dữ liệu về lương để AI dự đoán",key="data")
+setting={}
+if data is not None:
+    columns1, columns2 = st.columns(2)
+    byte_data=data.getvalue()
+    with open(f"./data/{data.name}","wb+") as f:
+        f.write(byte_data)
+
+    get_num_section = count_section()
+
+    dataset = ml.Dataset(f"./data/{data.name}")
+    
+    target = dataset.origin_data.columns[0]
+    st.subheader(f"Output feature: {target}")
+    st.subheader("Data")
+    st.dataframe(dataset.origin_data)
+
+    with st.sidebar:
+        if "select_all_status" not in st.session_state:
+            st.session_state["select_all_status"] = False
+        def set_select_all_status():
+            st.session_state.select_all_status = True
+        def reset_select_all_status():
+            st.session_state.select_all_status = False
+
+        get_feature_inputs={}
+        
+
+        #st.markdown(f"""## {get_num_section()}.Select features as input""")
+        st.subheader("Select features as input features")
+        if not st.session_state.select_all_status:
+            for i in list(dataset.origin_data.columns)[1:]:
+                get_feature_inputs.update({i:st.checkbox(f"{i}",key=f"feature_select_{i}")})
+        else:
+            get_feature_inputs={}
+            for i in list(dataset.origin_data.columns)[1:]:
+                get_feature_inputs.update({i:st.checkbox(f"{i}",key=f"feature_select_{i}",value=True)})
+        
+        colBut1, colBut2 =st.columns(2)
+        with colBut1:
+            st.button("Select all", on_click = set_select_all_status)
+        with colBut2:
+             st.button("Clear all", on_click = reset_select_all_status)
+        n_selected_features = np.sum(list(get_feature_inputs.values()))
+
+    # Select output feature
+    if "k_fold_select" not in st.session_state:
+        st.session_state["k_fold_select"]=False
+    def set_kflod():
+        st.session_state["k_fold_select"]=not st.session_state["k_fold_select"]
+        
+    if not st.session_state["k_fold_select"]:
+        col1, col2, col3= st.columns(3)
+        with col1:
+            # Select K-Fold
+            st.subheader(f"{get_num_section()}.Select K-Fold Cross validation")
+            kfold = st.checkbox(f"K-Fold Cross validation",key="kfold",on_change =set_kflod)
+            #st.session_state.k_fold_select=kfold
+            setting.update({"kfold":kfold})
+
+            if kfold:
+                if n_selected_features<3:
+                    st.error("Please select at least THREE features")
+                else:
+                    K = st.slider(f"Select K between 2 and {n_selected_features}",key="rate_train", step=1, min_value=2, max_value=int(n_selected_features), value=2)
+                    setting.update({"K":K})
+            # Select the rate of train dataset
+        with col2:
+            if kfold == False:
+                st.subheader(f"{get_num_section()}.Select Train/test split")
+                rate = st.slider("Enter the rate of train dataset",key="rate_train", value=0.1, min_value=0.1, max_value=1.0, step=0.01)
+                st.write(f"Train rate : {int(rate*100)}%")
+                st.write(f"Test rate : {int((1-rate)*100)}%")
+                setting.update({"rate":rate})
+        with col3:
+            # Select metric
+            st.subheader(f"{get_num_section()}.Select metrics")
+            st.write("Select at least one metric for evaluating model")
+            setting.update({"F1":st.checkbox(f"F1",key="F1")})
+            setting.update({"LogLoss":st.checkbox(f"LogLoss",key="LogLoss")})
+    else:
+        col1, col2 = st.columns(2)
+        with col1:
+            # Select K-Fold
+            st.subheader(f"{get_num_section()}.Select K-Fold Cross validation")
+            kfold = st.checkbox(f"K-Fold Cross validation",key="kfold",on_change =set_kflod)
+            setting.update({"kfold":kfold})
+
+            if kfold:
+                if n_selected_features<3:
+                    st.error("Please select at least THREE features")
+                else:
+                    K = st.slider(f"Select K between 2 and {n_selected_features}",key="rate_train", step=1, min_value=2, max_value=int(n_selected_features), value=2)
+                    setting.update({"K":K})
+            # Select the rate of train dataset
+        with col2:
+            # Select metric
+            st.subheader(f"{get_num_section()}.Select metrics")
+            st.write("Select at least one metric for evaluating model")
+            setting.update({"F1":st.checkbox(f"F1",key="F1")})
+            setting.update({"LogLoss":st.checkbox(f"LogLoss",key="LogLoss")})
+    # Select PCA
+    with st.container():
+        st.subheader(f"{get_num_section()}.Select PCA")
+        st.write(f"Do you want to use PCA for reducing demensions")
+        pca = st.checkbox(f"PCA",key="PCA")
+        if pca:
+            if n_selected_features<2:
+                st.error("Please select at lease a feature")
+            elif (setting["F1"] or setting["LogLoss"])==False:
+                st.error("Please select a metric for evaluation")
+            else:
+                feature_selected = [ i for i in get_feature_inputs.keys() if get_feature_inputs[i]]
+                setting.update({"feature_list":feature_selected,"target":target})
+                
+                fig, best = ml.search_PCA(dataset, setting, n_selected_features)
+                st.pyplot(fig)
+                reduce = st.slider(f"Select components bewteen 1 and {n_selected_features}",key="pca_n", step=1, min_value=1, max_value = int(n_selected_features), value=1)
+                setting.update({"pca_n":reduce})
+        setting.update({"pca":pca})
+    
+    if st.button("Run",key="run"):
+        if n_selected_features==0:
+            st.error("Please select at lease a feature")
+        elif (setting["F1"] or setting["LogLoss"])==False:
+            st.error("Please select a metric for evaluation")
+        else:
+            feature_selected = [ i for i in get_feature_inputs.keys() if get_feature_inputs[i]]
+            if len(feature_selected)<1:
+                st.error("Please select at least a feature")
+            else:
+                setting.update({"feature_list":feature_selected,"target":target})
+                model = ml.Model_AI(dataset, setting)
+                model.fit()
+
+                stats = model.get_value_metrics
+                c1,c2 = st.columns(2)
+                with c1:
+                    st.metric("F1 Score", "{v:.2f}".format(v=stats["f1"]))
+                with c2:
+                    st.metric("Log Loss", "{v:.2f}".format(v=stats["LogLoss"]))
+                st.pyplot(model.plot_history())
---
+++ App_Deploy/ml.py
@@ -0,0 +1,244 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, log_loss, f1_score
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import Normalizer
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler
+
+class OnehotEncoder:
+    def __init__(self):
+        self.lenght=0
+        self.data={}
+    def fit(self,data):
+        for id,key in enumerate(data):
+            self.data[key]=id
+        self.lenght = len(data)
+    def transform(self,data):
+        ba = []
+        for i in data:
+            a = np.zeros(self.lenght,dtype=np.int32)
+            for j in i:
+                a[self.data[j]]=1
+            ba.append(a.tolist())
+        return ba
+
+class Dataset:
+    def __init__(self,path_data,typedata="path"):
+        self.data=None
+        self.encoder={}
+        self.features = []
+        self.str_col=[]
+        self.target_encoder = None
+        if typedata=="df":
+            self.origin_data = path_data.copy()
+        else:
+            self.origin_data = pd.read_csv(path_data).sample(frac=1).reset_index(drop=True)
+        if len(self.origin_data.columns)==1:
+            self.origin_data = pd.read_table(path_data,sep=";")
+
+    def check_str_type(self, word):
+        return str(type(word)).split()[1].split("'")[1] == "str"
+
+    def process_data(self, data):
+        self.data = data.copy()
+        self.features.append(list(self.data.columns))
+        self.target = self.data.columns[-1]
+        for i in self.data.columns:
+            if self.check_str_type(self.data.iloc[0][i]):
+                LaEn = OnehotEncoder()
+                temp = self.data[i].unique()
+                LaEn.fit(temp)
+                self.str_col.append(i)
+                temp_data = np.array(LaEn.transform(self.data[i].values.reshape((-1,1)))).T
+                for id, key in enumerate(temp):
+                    self.data[key]=temp_data[id]
+                self.encoder.update({i:LaEn})
+                self.data.drop(i,axis=1,inplace=True)
+        self.features_value = list(filter(lambda x: x!=self.target, self.data.columns))
+        print(f"self.features_value:{self.features_value}")
+        return self.target, self.features_value
+
+    def get_feature_target(self,features_list,target,pca=False, pca_n=1):
+        features_list+=[target]
+        target_col,feature_col = self.process_data(self.origin_data[features_list])
+        if pca:
+            return self.get_pca_feature(pca_n,feature_col), self.data[[target_col]].values
+        return self.data[feature_col].values, self.data[[target_col]].values
+
+
+class Model_AI:
+    def __init__(self,dataset,setting):
+        self.data= dataset
+        self.setting = setting
+        self.AbsModel = RandomForestClassifier
+        self.history = {}
+        if self.setting["LogLoss"]:
+            self.history["LogLoss"]={}
+        if self.setting["F1"]:
+            self.history["F1"]={}
+
+    def fit(self):
+        X,y = self.data.get_feature_target(self.setting["feature_list"],self.setting["target"])
+        if self.setting["kfold"]:
+            kf = KFold(n_splits=self.setting["K"], shuffle=True)
+            fold_id=0
+            print(f"X={X}")
+            print(f"y={y}")
+            for train_index, test_index in kf.split(X):
+                scaler = MinMaxScaler()
+                xtrain, xtest = X[train_index], X[test_index]
+                xtrain = scaler.fit_transform(xtrain)
+                xtest = scaler.transform(xtest)
+
+                ytrain, ytest = y[train_index], y[test_index]
+
+                if self.setting["pca"]:
+                    PCA_Model = PCA(self.setting["pca_n"])
+                    xtrain = PCA_Model.fit_transform(xtrain)
+
+                Model = self.AbsModel()
+                Model.fit(xtrain,ytrain)
+                if self.setting["pca"]:
+                    xtest = PCA_Model.transform(xtest)
+                yhat = Model.predict(xtest)
+                yhat_p = Model.predict_proba(xtest)
+                #print(f"yhat1={yhat}")
+                #print(f"yhat_p1={yhat_p}")
+                if self.setting["LogLoss"]:
+                    self.history["LogLoss"].update({fold_id:log_loss(ytest,yhat_p)})
+                if self.setting["F1"]:
+                    self.history["F1"].update({fold_id:f1_score(ytest,yhat, average='weighted')})
+                fold_id+=1
+        else:
+            xtrain,xtest,ytrain,ytest = train_test_split(X,y, test_size = 1-self.setting["rate"])
+            Model = self.AbsModel()
+
+            if self.setting["pca"]:
+                PCA_Model = PCA(self.setting["pca_n"])
+                xtrain = PCA_Model.fit_transform(xtrain)
+
+            Model.fit(xtrain,ytrain)
+
+            if self.setting["pca"]:
+                xtest = PCA_Model.transform(xtest)
+
+            yhat = Model.predict(xtest)
+            yhat_p = Model.predict_proba(xtest)
+            print(f"yhat2={yhat}")
+            print(f"yhat_p2={yhat_p}")
+            if self.setting["LogLoss"]:
+                self.history["LogLoss"].update({0:log_loss(ytest,yhat_p)})
+            if self.setting["F1"]:
+                self.history["F1"].update({0:f1_score(ytest,yhat, average='weighted')})
+        self.model = Model
+        #print(self.history)
+    @property
+    def get_value_metrics(self):
+        return {"f1":np.mean(list(self.history["F1"].values())), "LogLoss":np.mean(list(self.history["LogLoss"].values()))}
+
+    def plot_history(self):
+        data = pd.DataFrame(self.history)
+        labels = list(data.index)
+        fig, ax = plt.subplots()
+        title = []
+        if len(labels)>1:
+            labels +=["Mean"]
+
+        x = np.arange(len(labels))  # the label locations
+        width = 0.35  # the width of the bars
+        try:
+            F1 = [i for i in data["F1"].values]
+            if "Mean" in labels:
+                F1 +=[np.mean(data["F1"].values)]
+            rects1= ax.bar(x - width/2, F1, width, label='F1', color="green")
+            title.append("F1")
+        except:
+            pass
+
+        try:
+            LogLoss = [i for i in data["LogLoss"].values]
+            if "Mean" in labels:
+                LogLoss += [np.mean(data["LogLoss"].values)]
+            rects2=ax.bar(x + width/2, LogLoss, width, label='LogLoss',color="blue")
+            title.append("LogLoss")
+        except:
+            pass
+        # Add some text for labels, title and custom x-axis tick labels, etc.
+        ax.set_ylabel('Value')
+        #ax.set_yscale("log")
+        ax.set_title(f'{title[0] if len(title)<1 else " and ".join(title)}')
+        ax.set_xticks(x, labels)
+        ax.legend()
+
+        #ax.bar_label(rects1, padding=3)
+        #ax.bar_label(rects2, padding=3)
+
+        fig.tight_layout()
+        return fig
+
+    def extract_vector(self,features):
+        feature_vector = []
+        for i in features.keys():
+            if i not in self.str_col:
+                feature_vector.append(features[i])
+        for i in self.str_col:
+            feature_vector.extend(self.encoder[i].transform([[features[i]]])[0])
+        return feature_vector
+
+    def predict(self, features):
+        '''
+            features = { Position:..., Level:...}
+        '''
+        features = self.extract_vector(features)
+        return self.best_model.predict([features]).reshape(1)[0]
+
+def search_PCA(dataset:Dataset,setting:dict, limit_component:int ) -> tuple:
+    history ={"f1":{},"log_loss":{}}
+    best = {"c":1,"f1":0}
+    setting = setting.copy()
+    for i in range(1,limit_component+1):
+        setting["pca"] = True
+        setting["pca_n"] = i
+        model = Model_AI(dataset,setting)
+        model.fit()
+        f1, log_loss = list(model.get_value_metrics.values())
+        history["f1"].update({i:f1})
+        history["log_loss"].update({i:log_loss})
+
+        if history["f1"][i]>best["f1"]:
+            best["c"] = i
+            best["f1"] = history["f1"][i]
+
+    data = pd.DataFrame(history)
+    labels = list(data.index)
+    fig, ax = plt.subplots()
+    title = []
+    try:
+        F1 = [i for i in data["f1"].values]
+        rects1= ax.bar(labels, F1, label='f1', color="green")
+        ax.scatter(labels, F1, label='f1', color="green")
+        title.append("f1")
+    except:
+        pass
+    try:
+        LogLoss = [i for i in data["log_loss"].values]
+        rects2=ax.plot(labels, LogLoss, label='log_loss',color="blue")
+        ax.scatter(labels, LogLoss, label='log_loss',color="blue")
+        title.append("log_loss")
+    except:
+        pass
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('F1 Score')
+    #ax.set_yscale("log")
+    ax.set_title("F1 score with different component")
+    #ax.set_xticks(x, labels)
+    ax.legend()
+    #ax.bar_label(rects1, padding=3)
+    #ax.bar_label(rects2, padding=3)
+    fig.tight_layout()
+    return fig,best
